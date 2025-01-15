0f2f6b6d17d61a5349154b17b6e6af3dd3ec5870
julia85310 <148855799+julia85310@users.noreply.github.com>
2025-01-15 11:14:12 +0100

    front-end apartado b

--- src/app/agenda/page.js
+++ src/app/agenda/page.js
@@ -16,12 +16,40 @@ export default function AgendaPage(){
         setContactos(data);
     }
 
+    async function eliminarContacto(contacto) {
+        const result = window.confirm(`¿Deseas eliminar a ${contacto.nombre} ${contacto.apellidos}?`);
+        if (!result) {
+            return;
+        }
+        try {
+            const res = await fetch("http://localhost:3000/api/agenda", {
+                method: 'DELETE',
+                headers: {
+                'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({id: contacto.id}),
+            });
+
+            const data = await res.json();
+
+            if (res.ok) {
+                fetchData();
+            } else {
+                alert(`Error: ${data.error}`);
+            }
+            
+        } catch (error) {
+            alert('Error estableciendo la conexión');
+        }
+    }
+
     return <div>
         <h1>Contactos</h1>
         <ul>
             {contactos.map((contacto) =>
                 <li key={contacto.id}>
                     {contacto.nombre} {contacto.apellidos}
+                    <button onClick={() => eliminarContacto(contacto)}></button>
                 </li>
             )}
         </ul>
